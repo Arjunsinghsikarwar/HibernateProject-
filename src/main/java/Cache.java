42849ea2c812690f27b351a4b8ccf18d83eeaa3d
Arjunsinghsikarwar <140879142+Arjunsinghsikarwar@users.noreply.github.com>
2025-05-24 00:23:24 +0530

    Update Cache.java

--- src/main/java/Cache.java
+++ src/main/java/Cache.java
@@ -1,39 +1,37 @@
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
 public class Cache {
     public static void main(String[] args) {
-        System.out.println("In this class see about caching Hibernate");
-        // We use cache taki jo query execute wo agr ek baar ho rhe hain toh wo cache jo ke memory store hain , uss me usska result save ho jaaye and jiske databse pr query ke jaada laod na ho.
-        // With help of the cache reponse time kaam hoota hain and load database pr kaam ho jaata hain.
+        System.out.println("This class demonstrates caching in Hibernate.");
 
-        Configuration configuration = new Configuration().configure().addAnnotatedClass(Side.class);  // here we put the class name in which entity class is.
+        // We use caching so that if a query is executed once, its result is stored in memory.
+        // This reduces the load on the database by retrieving the result from the cache instead of executing the query again.
+        // With the help of caching, response time is reduced and the database experiences less load.
+
+        Configuration configuration = new Configuration().configure().addAnnotatedClass(Side.class);  // Specify the entity class here.
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.openSession();
 
-        //Level1 cache by default hoota hain.
-        Side student = session.get(Side.class,12);
-        Side student1 = session.get(Side.class,12);  // By making the 2 object , the query would execute only for 1 time pnly even i we get two result print .
+        // Level 1 cache is enabled by default.
+        Side student = session.get(Side.class, 12);
+        Side student1 = session.get(Side.class, 12);  // Even though we fetch the object twice, the query will execute only once due to caching.
 
         System.out.println(student1);
-        System.out.println(student);    // cache seesion specific hoota  hain.
+        System.out.println(student);  // Level 1 cache is session-specific.
 
         session.close();
 
-        //But if change the Session the query would execute 2 times then.
+        // If we change the session, the query will execute again.
         Session session1 = sessionFactory.openSession();
 
-        // Remeber that , if even i after ehcache dependecies in pom.xml we have add some properties to make that work in the hibernate.cfg.xml file .
-        // Even after setup of the ehcaceh file , it will not work , to mae that we have the final step which add the @Cacheable notation on the class jishe humhe cache krna hain ke nahi.
+        // Remember: Even after adding Ehcache dependencies in pom.xml,
+        // we must also configure caching properties in the hibernate.cfg.xml file.
+        // Ehcache won't work just by adding the dependency. As a final step,
+        // we need to add the @Cacheable annotation to the entity class that we want to cache.
 
-
-
-        Side student2 = session1.get(Side.class,12);
-        System.out.println(student2);  // Since we have alreay that this in cache , but due change in session we the query will get execute and then we will gwt result.
-        session1.close();              // to resolve this use , we the Level2 cache by using the ehcache . (Remeber that we have use the same version of ehcache as hibernate).
-
+        Side student2 = session1.get(Side.class, 12);
+        System.out.println(student2);  // Although the data was cached, the query is executed again because we used a new session.
+        session1.close();              // To fix this, we use Level 2 caching with Ehcache.
+                                       // (Make sure to use a compatible version of Ehcache with Hibernate.)
 
     }
-} // After this even after session getting changed the query only get executed by only once.
+} // After enabling Level 2 cache, the query will only execute once even if the session changes.
 

72835fa2c0cc54d14f4437bcc49f14342d82a15c
Arjunsinghsikarwar <140879142+Arjunsinghsikarwar@users.noreply.github.com>
2025-05-24 00:24:11 +0530

    Update Fetch.java

--- src/main/java/Fetch.java
+++ src/main/java/Fetch.java
@@ -2,27 +2,29 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Fetch
-{
+public class Fetch {
     public static void main(String[] args) {
-        System.out.println("In this we fetch the data"); //By using hibernate.
-        Configuration configuration = new Configuration().configure().addAnnotatedClass(Side.class);  // here we put the class name in which entity class is.
+        System.out.println("In this example, we fetch data using Hibernate.");
+
+        Configuration configuration = new Configuration().configure().addAnnotatedClass(Side.class);  
+        // Here we specify the entity class.
+
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.openSession();
-        // Here we don't need to use the transaction , since it only uses to make the changes and update in the table.
 
-        // Remeber this , we have to method in session to fetch the data from the data base
-        // 1. get() -->  in this case if we get() the the query got executed  even if we used or not.
-        //2. load(). --> in this case if use the lod() the query only executed when only it got used.
+        // We don't need to use a transaction here since we're only fetching data, not making any changes or updates to the table.
 
-       Side student =  session.get(Side.class,12);
+        // Remember this: there are two methods in the Session interface to fetch data from the database:
+        // 1. get()  --> This method immediately executes the query, even if the retrieved object is not used.
+        // 2. load() --> This method executes the query only when the object is accessed (lazy loading).
+
+        Side student = session.get(Side.class, 12); // The query executes immediately.
 
         System.out.println(student);
 
-        session.close();  // just for the good prartice.
+        session.close();  // Closing the session is good practice.
 
-        // Side student = session.load(Side,class,102);
-        // this query will only exectue when it is used to get somethingh.
-
+        // Side student = session.load(Side.class, 102);
+        // This query will only execute when the object is accessed (e.g., via a getter).
     }
 }
